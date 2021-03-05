1686273eaf5e923011476769effc2dc0ab1639a2
Giuliano Sampaio <giuguitar@gmail.com>
2021-03-05 18:21:57 -0300

    creating drives

--- src/Concerns/HandlesCache.php
+++ src/Concerns/HandlesCache.php
@@ -4,13 +4,81 @@ namespace Maestriam\FileSystem\Concerns;
 
 use Illuminate\Support\Facades\Cache;
 
+/**
+ * Manipula a entrada/saída de itens no cache da aplicação
+ */
 trait HandlesCache
 {
-    private function cache(string $key)
+    /**
+     * Define/Retorna itens no cache do sistema
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return mixed
+     */
+    private function cache(string $key, $value = null)
     {
-        $exp  = "filesystem.default.%s";
-        $name = sprintf($exp, $key);
+        $name = $this->getCacheName($key);
 
+        if ($value == null) {
+            return $this->getCache($name);
+        } 
+                
+        return $this->setCache($name, $value);
+    }
+
+    /**
+     * Define/Retorna os itens no cache, definidos como padrão
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return mixed
+     */
+    public function default(string $key, $value = null)
+    {
+        $name = $this->getCacheName($key, 'filesystem.default');
+
+        if ($value == null) {
+            return $this->getCache($name);
+        } 
+                
+        return $this->setCache($name, $value);
+    }
+
+    /**
+     * Gera uma chave de acesso para item no cache
+     *
+     * @param string $key
+     * @param string $prefix
+     * @return string
+     */
+    private function getCacheName(string $key, string $prefix = null) : string 
+    {
+        $prefix = ($prefix == null) ? $this->name() : $prefix;        
+
+        return sprintf("%s.%s", $prefix, $key);
+    }    
+    
+    /**
+     * Retorna um item do cache
+     *
+     * @param string $name
+     * @return mixed
+     */
+    private function getCache(string $name)
+    {
         return Cache::get($name);
+    }
+
+    /**
+     * Define um item no cache
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return mixed
+     */
+    private function setCache(string $name, $value)
+    {        
+        return Cache::put($name, $value);
     }
 }
--- src/Entities/Drive.php
+++ src/Entities/Drive.php
@@ -2,16 +2,18 @@
 
 namespace Maestriam\FileSystem\Entities;
 
+use Maestriam\FileSystem\Entities\File;
 use Maestriam\FileSystem\Concerns\FluentCalls;
 use Maestriam\FileSystem\Concerns\HandlesCache;
-use Maestriam\FileSystem\Entities\File;
-use Illuminate\Support\Facades\Cache;
 use Maestriam\FileSystem\Contracts\DriveInterface;
 
 class Drive implements DriveInterface
 {
     use FluentCalls, HandlesCache;
 
+    /**
+     * Caminho do diretório para criação de arquivos
+     */
     private string $rootFolder;
 
     public function __construct(string $name)
@@ -19,76 +21,128 @@ class Drive implements DriveInterface
         $this->name($name);
     }
 
-
+    /**
+     * Retorna a instância de manipulação de arquivos
+     *
+     * @param string $name
+     * @return File
+     */
     public function file(string $name) : File
     {
         return new File($name);    
     }
 
+    /**
+     * Retorna a instância de manipulação de diretórios
+     *
+     * @param string $name
+     * @return Folder
+     */
     public function folder(string $name) : Folder
     {
         return new Folder($name);
     }
 
+    /**
+     * Salva as configurações definidas para o drive
+     *
+     * @return Drive
+     */
     public function save() : Drive
     {
-        $this->setCache('root', $this->root());
-        $this->setCache('template', $this->template());
-        $this->setCache('structure', $this->structure());      
+        $this->cache('root', $this->root());
+        $this->cache('template', $this->template());
+        $this->cache('structure', $this->structure());      
         
         return $this;
     }
 
+    /**
+     * Define o nome do drive
+     *
+     * @param string $name
+     * @return Drive
+     */
     private function setName(string $name) : Drive
     {
         $this->name = strtolower($name);
         return $this;
     }
 
+    /**
+     * Retorna o nome do drive
+     *
+     * @return string
+     */
     private function getName() : string
     {
         return $this->name;
     }
 
+    /**
+     * Define o caminho de templates do drive
+     *
+     * @param string $folder
+     * @return Drive
+     */
     private function setTemplate(string $folder) : Drive
     {
         $this->templateFolder = $folder;
         return $this;
     }
 
+    /**
+     * Retorna o caminho de templates do drive
+     *
+     * @return string
+     */
     private function getTemplate() : string
     {
-        $cached = Cache::get('filesystem.default.template');
-
-        return $this->templateFolder ?? Cache::get('filesystem.default.template');
+        return $this->templateFolder ?? $this->default('template');
     }
 
+    /**
+     * Define a estrutura de diretórios de criação de arquivos gerados
+     *
+     * @param array $structure
+     * @return Drive
+     */
     private function setStructure(array $structure) : Drive
     {
         $this->structure = $structure;
         return $this;
     }
     
+    /**
+     * Retorna a estrutura de diretórios de criação de arquivos gerados
+     *
+     * @param array $structure
+     * @return Drive
+     */
     private function getStructure() : array
     {
-        return $this->structure ?? Cache::get('filesystem.default.structure');
+        return $this->structure ?? $this->default('structure');
     }
 
+    /**
+     * Define o caminho de destino dos arquivos gerados
+     *
+     * @param string $rootFolder
+     * @return Drive
+     */
     private function setRoot(string $rootFolder) : Drive
     {
         $this->rootFolder = $rootFolder;
         return $this;
     }
     
+    /**
+     * Retorna o caminho de destino dos arquivos gerados
+     *
+     * @return string
+     */
     private function getRoot() : string
     {
-        return $this->rootFolder ?? Cache::get('filesystem.default.root');
-    }
-
-    private function setCache(string $key, $value)
-    {
-        $cache = sprintf("%s.%s", $this->name(), $key);
-      
-        Cache::put($cache, $value);
+        return $this->rootFolder ?? $this->default('root');
     }
 }
