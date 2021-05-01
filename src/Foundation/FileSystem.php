d9a94b4c76f65ec5bb9cc26f5acbaf9517e3d14a
Giuliano Sampaio <giuguitar@gmail.com>
2021-05-01 14:03:38 -0300

    adding sanitize path function

--- src/Foundation/FileSystem.php
+++ src/Foundation/FileSystem.php
@@ -3,11 +3,17 @@
 namespace Maestriam\FileSystem\Foundation;
 
 use Maestriam\FileSystem\Foundation\Drive;
+use Maestriam\FileSystem\Foundation\Drive\PathSanitizer;
 
 class FileSystem
 {
     public function drive(string $name) : Drive
     {
         return new Drive($name);
+    }
+
+    public function sanitize(string $path) : string
+    {
+        return PathSanitizer::sanitize($path);
     }
 }
